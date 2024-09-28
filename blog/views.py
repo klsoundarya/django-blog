ccd3cb590bb36301a5df51699a5a7e0a531dc5f4
klsoundarya <k.soundarya7@gmail.com>
2024-09-28 09:15:20 +0100

    remove home func to see if error is not ocurring

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to the Home Page")
-
 def my_blog(request):
     return HttpResponse("Hello, Blog!")
 # Create your views here.
